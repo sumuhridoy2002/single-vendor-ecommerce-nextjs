2233aac8dea8de74505ec1012ea43b52897c720e
mahadih-gg <mahadih.dev@gmail.com>
2026-01-20 06:15:27 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+import Provider from "@/components/global/Provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider>
+          {children}
+        </Provider>
       </body>
     </html>
   );
